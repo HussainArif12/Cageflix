6a1d893867a370a56a9722d5987047edcf43c57f
Hussain Arif <hussainarifkl@gmail.com>
2025-04-10 11:39:36 +0200

    Buttons fully clickable

--- src/components/RowDisplay.tsx
+++ src/components/RowDisplay.tsx
@@ -19,28 +19,26 @@ const RowDisplay: React.FC<RowDisplayProps> = ({ movies }) => {
       ref={navRef}
     >
       <div>
-        <button className="absolute cursor-pointer hover:opacity-100 bg-slate-300 opacity-70 py-10 left-10 z-10 text-black my-[5rem]">
-          <ArrowLeftCircleIcon
-            height={36}
-            width={36}
-            onClick={() => {
-              navRef.current?.scrollBy({ left: -100, behavior: "smooth" });
-            }}
-          />
+        <button
+          onClick={() => {
+            navRef.current?.scrollBy({ left: -100, behavior: "smooth" });
+          }}
+          className="absolute cursor-pointer hover:opacity-100 bg-slate-300 opacity-70 py-10 left-10 z-10 text-black my-[5rem]"
+        >
+          <ArrowLeftCircleIcon height={36} width={36} />
         </button>
       </div>
       {movies.map((item) => (
         <MovieDisplay key={item.tconst} movie={item} />
       ))}
       <div>
-        <button className="absolute cursor-pointer bg-slate-300 hover:opacity-100 opacity-70 py-10 right-8 z-10 text-black my-[5rem]">
-          <ArrowRightCircleIcon
-            height={36}
-            width={36}
-            onClick={() => {
-              navRef.current?.scrollBy({ left: 100, behavior: "smooth" });
-            }}
-          />
+        <button
+          onClick={() => {
+            navRef.current?.scrollBy({ left: 100, behavior: "smooth" });
+          }}
+          className="absolute cursor-pointer bg-slate-300 hover:opacity-100 opacity-70 py-10 right-8 z-10 text-black my-[5rem]"
+        >
+          <ArrowRightCircleIcon height={36} width={36} />
         </button>
       </div>
     </div>
